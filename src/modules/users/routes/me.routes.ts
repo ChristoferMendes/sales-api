23a56f274baa478c6e49083a7289a474a9fd96df
ChristoferMendes <christoferluizdsm@gmail.com>
2022-12-26 17:40:10 -0300

    returning avatar_url with instanceToInstance

--- src/modules/users/routes/me.routes.ts
+++ src/modules/users/routes/me.routes.ts
@@ -1,6 +1,7 @@
 import { isAuthenticated } from '@shared/http/middlewares/isAuthenticated';
 import { Router } from 'express';
 import { UserRepository } from '../repositories/user.repository';
+import { instanceToInstance } from 'class-transformer';
 
 export const meRouter = Router();
 meRouter.use(isAuthenticated);
@@ -10,5 +11,5 @@ meRouter.post('/', async (req, res) => {
 
   const user = await UserRepository.findOneBy({ uuid });
 
-  return res.status(200).json(user);
+  return res.status(200).json(instanceToInstance(user));
 });
