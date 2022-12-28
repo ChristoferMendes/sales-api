746a346c7b438140cbcc095cba1ed6972a0f934d
ChristoferMendes <christoferluizdsm@gmail.com>
2022-12-28 14:00:08 -0300

    added a notFound clause

--- src/modules/users/controllers/user_email.controller.ts
+++ src/modules/users/controllers/user_email.controller.ts
@@ -3,14 +3,19 @@ import { Request, Response } from 'express';
 import { UserEmailService } from '../services/user_email.service';
 
 const userEmailService = new UserEmailService();
-const { ok } = new Status();
+const { ok, notFound } = new Status();
 
 export class UserEmailController {
   async execute(req: Request, res: Response) {
     const { email } = req.body;
 
     const user = await userEmailService.execute({ email });
-    console.log(user);
+
+    if (!user) {
+      return res.status(notFound).json({
+        message: 'This user does not exist',
+      });
+    }
 
     return res.status(ok).json(user);
   }
